b1732c04b8bf227caa86c15b501717e434a77e86
Ross Buggins (NHS) <ross.buggins@nhs.net>
2024-05-29 20:28:12 +0000

    zsh in post create

--- .devcontainer/postcreatecommand.sh
+++ .devcontainer/postcreatecommand.sh
@@ -5,5 +5,6 @@ git clone https://github.com/asdf-vm/asdf.git ~/.asdf;
 chmod +x ~/.asdf/asdf.sh;
 echo '. $HOME/.asdf/asdf.sh' >> ~/.zshrc
 echo '. $HOME/.asdf/completions/asdf.bash' >> ~/.zshrc
+zsh
 
 echo 'asdf setup complete'
