20c2f54b5cda52ab268ec3699518f3e10f58d48a
aidenvaines-cgi <aiden.vaines@cgi.com>
2026-04-08 17:10:30 +0100

    CCM-16446 Adding repo support and pre-commit fixes

--- scripts/docker/docker.lib.sh
+++ scripts/docker/docker.lib.sh
@@ -430,7 +430,7 @@ function docker-push-container() {
 function docker-calculate-image-name() {
   local dir=${dir:-$PWD}
   local container_name="${CONTAINER_NAME:-$(basename "$dir")}"
-  local ecr_repo="${ECR_REPO:-nhs-main-acct-admail}"
+  local ecr_repo="${ECR_REPO:-nhs-main-acct-main}"
   local image_suffix="${CONTAINER_IMAGE_SUFFIX:-$(docker-get-git-version-suffix)}"
   local image_tag="${CONTAINER_IMAGE_PREFIX}-${container_name}"
   local ecr_repo_uri="${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_REGION}.amazonaws.com/${ecr_repo}"
