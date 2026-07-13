a59b0f6c4ad9afca1065f4f5d50412a1132fd554
aidenvaines-cgi <aiden.vaines@cgi.com>
2026-07-13 16:39:23 +0100

    CCM-18536 adding ASDF packages

--- src/jekyll-devcontainer/src/.devcontainer/nhsnotify/install.sh
+++ src/jekyll-devcontainer/src/.devcontainer/nhsnotify/install.sh
@@ -7,8 +7,6 @@ if [[ ! -d /.asdf ]]; then
     git clone https://github.com/asdf-vm/asdf.git /.asdf --branch v0.18.0
 fi
 
-chmod +x /.asdf/asdf.sh
-grep -qxF '. /.asdf/asdf.sh' /.zshrc || echo '. /.asdf/asdf.sh' >> /.zshrc
 grep -qxF '. /.asdf/completions/asdf.bash' /.zshrc || echo '. /.asdf/completions/asdf.bash' >> /.zshrc
 sed -i "/plugins=/c\plugins=(git ssh-agent sudo terraform dirhistory zsh-autosuggestions)" /.zshrc
 
