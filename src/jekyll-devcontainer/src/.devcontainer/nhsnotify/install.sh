774365911136a3a86c4fbc6891024b91518c5ae9
Aiden Vaines <54067008+aidenvaines-cgi@users.noreply.github.com>
2026-07-14 11:42:09 +0100

    CCM-18536 fixing ASDF packages (#254)

--- src/jekyll-devcontainer/src/.devcontainer/nhsnotify/install.sh
+++ src/jekyll-devcontainer/src/.devcontainer/nhsnotify/install.sh
@@ -3,17 +3,6 @@
 cp ./postcreatecommand.sh /postcreatecommand.sh
 
 cp ~/.zshrc /.zshrc
-if [[ ! -d /.asdf ]]; then
-    git clone https://github.com/asdf-vm/asdf.git /.asdf --branch v0.18.0
-fi
-
-# Remove any existing home-based asdf init lines to avoid switching to /home/vscode/.asdf.
-sed -i "/\. '\$HOME\/.asdf\/completions\/asdf.bash'/d" /.zshrc
-sed -i '/\. \/\.asdf\/completions\/asdf.bash/d' /.zshrc
-
-grep -qxF 'export ASDF_DIR=/.asdf' /.zshrc || echo 'export ASDF_DIR=/.asdf' >> /.zshrc
-grep -qxF 'export ASDF_DATA_DIR=/.asdf' /.zshrc || echo 'export ASDF_DATA_DIR=/.asdf' >> /.zshrc
-grep -qxF 'export PATH=/.asdf/bin:/.asdf/shims:$PATH' /.zshrc || echo 'export PATH=/.asdf/bin:/.asdf/shims:$PATH' >> /.zshrc
 sed -i "/plugins=/c\plugins=(git ssh-agent sudo terraform dirhistory zsh-autosuggestions)" /.zshrc
 
 cat /.zshrc
